f6dd44262342df628730e1859c58a895213dee6b
goetzmat <magoe1997@gmail.com>
2021-06-24 14:08:53 +0200

    [Feature] Around Car+Truck starts driving when pressed a second time

--- scripts/situations/aroundCar.py
+++ scripts/situations/aroundCar.py
@@ -4,26 +4,29 @@ import carla
 import time
 from numpy import random
 
+car_lst = None
 
 def start(world,client,traffic_manager):
+    global car_lst
     SpawnActor = carla.command.SpawnActor
     SetAutopilot = carla.command.SetAutopilot
-    print("lib:Starting Situation around Car")
-    spawn_points = world.get_map().get_spawn_points()
-    blueprints = world.get_blueprint_library().filter('vehicle.*')
-    car_bp = random.choice(blueprints.filter('vehicle.audi.etron'))
-    if car_bp.has_attribute('driver_id'):
-        driver_id = random.choice(car_bp.get_attribute('driver_id').recommended_values)
-        car_bp.set_attribute('driver_id', driver_id)
-    car = SpawnActor(car_bp, spawn_points[114])
-    car_lst = client.apply_batch_sync([car], False)
-    # Time until Car starts driving
-    time.sleep(10)
-    
-    batch = []
-    print("Done")
-    batch.append(SetAutopilot(car_lst[0].actor_id, True, traffic_manager.get_port()))
-    client.apply_batch_sync(batch)
-    car_act = world.get_actor(car_lst[0].actor_id)
+    if car_lst == None:
+        print("lib:Starting Situation around Car")
+        spawn_points = world.get_map().get_spawn_points()
+        blueprints = world.get_blueprint_library().filter('vehicle.*')
+        car_bp = random.choice(blueprints.filter('vehicle.audi.etron'))
+        if car_bp.has_attribute('driver_id'):
+            driver_id = random.choice(car_bp.get_attribute('driver_id').recommended_values)
+            car_bp.set_attribute('driver_id', driver_id)
+        car = SpawnActor(car_bp, spawn_points[114])
+        car_lst = client.apply_batch_sync([car], False)
+        car_act = world.get_actor(car_lst[0].actor_id)
+    else:    
+        batch = []
+        print("Car starting to drive")
+        batch.append(SetAutopilot(car_lst[0].actor_id, True, traffic_manager.get_port()))
+        client.apply_batch_sync(batch)
+        car_act = world.get_actor(car_lst[0].actor_id)
+        car_lst=None
     return car_act
 
--- scripts/situations/aroundTruck.py
+++ scripts/situations/aroundTruck.py
@@ -4,28 +4,32 @@ import carla
 import time
 from numpy import random
 
+truck_lst = None
+
 def start(world,client,traffic_manager):
+    global truck_lst
     SpawnActor = carla.command.SpawnActor
     SetAutopilot = carla.command.SetAutopilot
-    print("lib:Starting Situation around Truck")
-    spawn_points = world.get_map().get_spawn_points()
-    blueprints = world.get_blueprint_library().filter('vehicle.*')
-    truck_bp = random.choice(blueprints.filter('vehicle.carlamotors.carlacola'))
-    if truck_bp.has_attribute('driver_id'):
-        driver_id = random.choice(truck_bp.get_attribute('driver_id').recommended_values)
-        truck_bp.set_attribute('driver_id', driver_id)
-    truck = SpawnActor(truck_bp, spawn_points[63])
-    truck_lst = client.apply_batch_sync([truck], False)
-    truck_act = world.get_actor(truck_lst[0].actor_id)
-    if truck_act is not None:
-        truck_act.apply_control(carla.VehicleControl(brake=1.0))
+    if truck_lst == None:
+        print("lib:Starting Situation around Truck")
+        spawn_points = world.get_map().get_spawn_points()
+        blueprints = world.get_blueprint_library().filter('vehicle.*')
+        truck_bp = random.choice(blueprints.filter('vehicle.carlamotors.carlacola'))
+        if truck_bp.has_attribute('driver_id'):
+            driver_id = random.choice(truck_bp.get_attribute('driver_id').recommended_values)
+            truck_bp.set_attribute('driver_id', driver_id)
+        truck = SpawnActor(truck_bp, spawn_points[63])
+        truck_lst = client.apply_batch_sync([truck], False)
+        truck_act = world.get_actor(truck_lst[0].actor_id)
+        if truck_act is not None:
+            truck_act.apply_control(carla.VehicleControl(brake=1.0))
+    else:
+        batch = []
+        print("Truck starting to drive")
+        batch.append(SetAutopilot(truck_lst[0].actor_id, True, traffic_manager.get_port()))
+        client.apply_batch_sync(batch)
+        truck_act = world.get_actor(truck_lst[0].actor_id)
+        truck_lst = None
 
-    # Time until Truck starts driving
-    time.sleep(10)
-    
-    batch = []
-    print("Done")
-    batch.append(SetAutopilot(truck_lst[0].actor_id, True, traffic_manager.get_port()))
-    client.apply_batch_sync(batch)
     return truck_act
 
